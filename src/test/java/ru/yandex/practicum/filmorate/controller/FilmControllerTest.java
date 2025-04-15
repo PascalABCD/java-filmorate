c0b007a072394fcd32e602cd6f81176682d0ea07
Alexandra Azargaeva <aleksandra.azargaeva@dif.tech>
2025-04-16 02:03:02 +0500

    [add-friends-likes] fix

--- src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
@@ -6,8 +6,6 @@ import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.service.FilmService;
 import ru.yandex.practicum.filmorate.storage.FilmStorage;
 import ru.yandex.practicum.filmorate.storage.InMemoryFilmStorage;
-import ru.yandex.practicum.filmorate.storage.InMemoryUserStorage;
-import ru.yandex.practicum.filmorate.storage.UserStorage;
 
 import java.time.LocalDate;
 
@@ -15,8 +13,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class FilmControllerTest {
     FilmStorage filmStorage = new InMemoryFilmStorage();
-    UserStorage userStorage = new InMemoryUserStorage();
-    FilmService filmService = new FilmService(filmStorage, userStorage);
+    FilmService filmService = new FilmService(filmStorage);
     FilmController filmController = new FilmController(filmService);
     LocalDate releaseDate = LocalDate.of(2023, 1, 1);
 
